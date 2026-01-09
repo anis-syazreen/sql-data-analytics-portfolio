a10be21cc8eec0f1976d12c65597f1f36e89772f
Anis Syazreen <anissyazreen23@gmail.com>
2026-01-09 10:39:03 +0800

    Update data_cleaning.sql

--- sql_scripts/data_cleaning.sql
+++ sql_scripts/data_cleaning.sql
@@ -1,1 +1,10 @@
+-- Check for unresolved complaints
+SELECT *
+FROM ict_complaints
+WHERE status <> 'Resolved';
+
+-- Identify long resolution cases
+SELECT *
+FROM ict_complaints
+WHERE days_to_resolve > 7;
 
