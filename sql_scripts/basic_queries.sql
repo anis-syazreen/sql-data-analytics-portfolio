e29973a0354f8e6c717acb4aa255845c02fda4fb
Anis Syazreen <anissyazreen23@gmail.com>
2026-01-09 10:37:56 +0800

    Update basic_queries.sql

--- sql_scripts/basic_queries.sql
+++ sql_scripts/basic_queries.sql
@@ -1,1 +1,14 @@
+-- View all sales records
+SELECT *
+FROM sales_data;
+
+-- Filter sales from Central region
+SELECT *
+FROM sales_data
+WHERE region = 'Central';
+
+-- Calculate total sales value
+SELECT
+    SUM(quantity * unit_price) AS total_sales
+FROM sales_data;
 
