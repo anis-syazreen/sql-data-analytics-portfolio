d2eb1836f92b80ce63163c85ab5819d73f4c62db
Anis Syazreen <anissyazreen23@gmail.com>
2026-01-09 10:38:42 +0800

    Update joins_analysis.sql

--- sql_scripts/joins_analysis.sql
+++ sql_scripts/joins_analysis.sql
@@ -1,1 +1,8 @@
+-- Example join analysis (conceptual)
+SELECT
+    s.customer_id,
+    s.region,
+    SUM(s.quantity * s.unit_price) AS total_spent
+FROM sales_data s
+GROUP BY s.customer_id, s.region;
 
